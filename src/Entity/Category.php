a58bf3a0dd4f08d0f32f21277df2b7231710da3e
Aymerick Cotche <aymerick.cotche@hotmail.fr>
2023-07-12 14:07:48 +0400

    project done

---
+++ src/Entity/Category.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    use TimestampableEntity;
+    use BlameableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Joke::class)]
+    private Collection $jokes;
+
+    public function __construct()
+    {
+        $this->jokes = new ArrayCollection();
+    }
+
+    public function __toString()
+    {
+      return $this->name;
+    }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Joke>
+     */
+    public function getJokes(): Collection
+    {
+        return $this->jokes;
+    }
+
+    public function addJoke(Joke $joke): static
+    {
+        if (!$this->jokes->contains($joke)) {
+            $this->jokes->add($joke);
+            $joke->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeJoke(Joke $joke): static
+    {
+        if ($this->jokes->removeElement($joke)) {
+            // set the owning side to null (unless already changed)
+            if ($joke->getCategory() === $this) {
+                $joke->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
